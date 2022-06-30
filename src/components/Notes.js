9d5cfd173254b9458a290d860171c3efcdf9dedc
Alejo9601 <alejandro96jm@gmail.com>
2022-06-30 16:53:55 -0300

    Added tittle and Form, modified styles

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,13 +1,25 @@
 import notes from "../assets/notas_prueba.json";
 import Note from "./Note";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  font-size: 3rem;
+`;
 
 const Notes = () => {
   return (
-    <ol>
-      {notes.map((note, index) => (
-        <Note key={index} content={note.content} date={note.date} />
-      ))}
-    </ol>
+    <div>
+      <StyledH1>Notes</StyledH1>
+      <ol>
+        {notes.map((note, index) => (
+          <Note key={index} content={note.content} date={note.date} />
+        ))}
+      </ol>
+      <form>
+        <input type="text"></input>
+        <button>Create note</button>
+      </form>
+    </div>
   );
 };
 
